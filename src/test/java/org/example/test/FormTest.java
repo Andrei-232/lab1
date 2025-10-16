9157da31ccad5223f225eeede8df362cac6edd95
Andrei Capcelea <acapcelea06@gmai.com>
2025-10-16 09:58:40 +0300

    gitRemoteDriver

--- src/test/java/org/example/test/FormTest.java
+++ src/test/java/org/example/test/FormTest.java
@@ -32,7 +32,7 @@ public class FormTest {
     @BeforeClass
     public void beforeTest() {
         logger.info("Start beforeTest");
-        driver = Driver.getAutoLocalDriver();
+        driver = Driver.getRemoteDriver();
         driver.manage().window().maximize();
     }
 
